486ab3f86031d086972e1f53bcb3f1e54a364c66
parulsh239 <66636856+parulsh239@users.noreply.github.com>
2025-08-28 14:18:01 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,3 +20,23 @@ self.addEventListener('fetch', event => {
   );
 });
 
+// FAQ functionality
+function toggleFaq(questionElement) {
+    const answer = questionElement.nextElementSibling;
+    const toggle = questionElement.querySelector('.faq-toggle');
+    
+    if (answer.classList.contains('show')) {
+        answer.classList.remove('show');
+        toggle.classList.remove('rotated');
+    } else {
+        // Close all other FAQs
+        document.querySelectorAll('.faq-answer.show').forEach(openAnswer => {
+            openAnswer.classList.remove('show');
+            openAnswer.previousElementSibling.querySelector('.faq-toggle').classList.remove('rotated');
+        });
+        
+        // Open this FAQ
+        answer.classList.add('show');
+        toggle.classList.add('rotated');
+    }
+}
